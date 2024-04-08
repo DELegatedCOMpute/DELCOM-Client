1be0566b9f62fbd3e1c25b4307ba210d7f31e238
Andrew VanNus <avannus@gmail.com>
2024-04-08 13:40:50 -0400

    prettier autoformat

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import fs from 'node:fs';
 import fsp from 'node:fs/promises';
 import path from 'node:path';
 import os from 'node:os';
-import {io, Socket} from 'socket.io-client';
-import {spawn} from 'child_process';
+import { io, Socket } from 'socket.io-client';
+import { spawn } from 'child_process';
 
 const outputNames = [
   'build_std_out',
@@ -12,49 +12,45 @@ const outputNames = [
   'run_std_err',
 ];
 
-type callbackWithErr = (arg0?: {err: string}) => void;
+type callbackWithErr = (arg0?: { err: string }) => void;
 
 type workerListElement = {
-  id: string,
-}
+  id: string;
+};
 
 type jobInfoType = {
-  dir: fs.PathLike,
-  writeStreams: {[key: string]: fs.WriteStream},
-}
+  dir: fs.PathLike;
+  writeStreams: { [key: string]: fs.WriteStream };
+};
 
 type resultInfoType = {
-  dir: fs.PathLike,
-  writeStreams: {[key: string]: fs.WriteStream},
+  dir: fs.PathLike;
+  writeStreams: { [key: string]: fs.WriteStream };
   finishPromise: {
-    promise?: Promise<void>,
-    res?: () => void,
-    rej?: (arg0?: unknown) => void,
-  },
-}
+    promise?: Promise<void>;
+    res?: () => void;
+    rej?: (arg0?: unknown) => void;
+  };
+};
 
 type configType = {
-  ip: string, // ip of server
-  port: number, // port of server
-  socket?: Socket, // socket to server
-  id?: string, // unique ID
-  delcomTempDir?: string,
-  isWorking: boolean, // if the client is currently working
-  isDelegating: boolean, // if the client is currently working
-  isWorker: boolean, // if the client is willing to work
-  job?: jobInfoType, // info about current job
-  res?: resultInfoType // info about job results
-}
+  ip: string; // ip of server
+  port: number; // port of server
+  socket?: Socket; // socket to server
+  id?: string; // unique ID
+  delcomTempDir?: string;
+  isWorking: boolean; // if the client is currently working
+  isDelegating: boolean; // if the client is currently working
+  isWorker: boolean; // if the client is willing to work
+  job?: jobInfoType; // info about current job
+  res?: resultInfoType; // info about job results
+};
 
 export default class Delcom {
-
   private _config: configType;
 
-  constructor(args: {
-    ip: string,
-    port: number,
-  }) {
-    const {ip, port} = args;
+  constructor(args: { ip: string; port: number }) {
+    const { ip, port } = args;
     this._config = {
       ip,
       port,
@@ -94,30 +90,38 @@ export default class Delcom {
             throw Error('No temp dir!');
           }
           this._config.job = {
-            dir: await fsp.mkdtemp(`${this._config.delcomTempDir}${path.sep}job`),
+            dir: await fsp.mkdtemp(
+              `${this._config.delcomTempDir}${path.sep}job`
+            ),
             writeStreams: {},
           };
-          console.log(`New job files will be stored at ${this._config.job.dir}`);
+          console.log(
+            `New job files will be stored at ${this._config.job.dir}`
+          );
           for (const fileName of fileNames) {
             this._config.job.writeStreams[fileName] = fs.createWriteStream(
               `${this._config.job.dir}${path.sep}${fileName}`,
-              {encoding: 'base64'},
+              { encoding: 'base64' }
             );
           }
           callback();
         } catch (err) {
           this.clearJob(err, callback);
         }
-      });
+      }
+    );
 
     socket.on(
       'receive_file_data_ack',
-      (data: {name: string, chunk: string | Buffer}, callback: callbackWithErr) => {
+      (
+        data: { name: string; chunk: string | Buffer },
+        callback: callbackWithErr
+      ) => {
         try {
           if (!this._config.job) {
             throw Error('No job setup to write to!');
           }
-          const {name, chunk} = data;
+          const { name, chunk } = data;
           const jobWS = this._config.job.writeStreams;
           jobWS[name].write(chunk, (err) => {
             if (err) {
@@ -129,63 +133,53 @@ export default class Delcom {
         } catch (err) {
           this.clearJob(err, callback);
         }
-      });
+      }
+    );
 
     for (const outputName of outputNames) {
-      socket.on(
-        `${outputName}`,
-        async (chunk, callback: callbackWithErr) => {
-          try {
-            const ws = this._config.res?.writeStreams[outputName];
-            if (!ws) {
-              throw Error('No writeable stream to write to!');
-            }
-            ws.write(chunk);
-          } catch (err) {
-            this.clearJob(err, callback);
-          }
-        });
-    }
-
-    socket.on(
-      'run_job_ack',
-      async (callback: callbackWithErr) => {
+      socket.on(`${outputName}`, async (chunk, callback: callbackWithErr) => {
         try {
-        // TODO ensure write streams are drained, clear them
-          console.log(`starting job at ${this._config.res?.dir}`);
-          await this.buildContainer();
-          console.log('built job, running');
-          await this.runContainer();
-          console.log('finished job');
-          callback();
-          socket.emit('done');
-          this.clearJob();
+          const ws = this._config.res?.writeStreams[outputName];
+          if (!ws) {
+            throw Error('No writeable stream to write to!');
+          }
+          ws.write(chunk);
         } catch (err) {
           this.clearJob(err, callback);
         }
       });
+    }
 
-    socket.on(
-      'finished',
-      () => {
-        this.clearDelegation();
-      });
+    socket.on('run_job_ack', async (callback: callbackWithErr) => {
+      try {
+        // TODO ensure write streams are drained, clear them
+        console.log(`starting job at ${this._config.res?.dir}`);
+        await this.buildContainer();
+        console.log('built job, running');
+        await this.runContainer();
+        console.log('finished job');
+        callback();
+        socket.emit('done');
+        this.clearJob();
+      } catch (err) {
+        this.clearJob(err, callback);
+      }
+    });
 
-    socket.on(
-      'get_config_ack',
-      (callback: (arg0: configType) => void) => {
-        callback(this._config);
-      });
+    socket.on('finished', () => {
+      this.clearDelegation();
+    });
 
-    socket.on(
-      'disconnect',
-      (reason) => {
-        console.log(`Disconnected: ${reason}`);
-      });
+    socket.on('get_config_ack', (callback: (arg0: configType) => void) => {
+      callback(this._config);
+    });
 
-    return new Promise<void>((res) => socket.on(
-      'connect',
-      async () => {
+    socket.on('disconnect', (reason) => {
+      console.log(`Disconnected: ${reason}`);
+    });
+
+    return new Promise<void>((res) =>
+      socket.on('connect', async () => {
         console.log('connected');
         this._config.id = await socket.emitWithAck('identify', {
           id: this._config.id,
@@ -193,14 +187,15 @@ export default class Delcom {
           isWorker: this._config.isWorker,
         });
         res();
-      }));
+      })
+    );
   }
 
   /**
    *
    * @returns A void promise on success
    */
-  async joinWorkforce(): Promise<void | {err: unknown}> {
+  async joinWorkforce(): Promise<void | { err: unknown }> {
     try {
       this._config.isWorker = true;
       if (!this._config.socket) {
@@ -208,11 +203,11 @@ export default class Delcom {
       }
       await this._config.socket.emitWithAck('join_ack');
     } catch (err) {
-      return {err};
+      return { err };
     }
   }
 
-  async leaveWorkforce(): Promise<void | {err: unknown}> {
+  async leaveWorkforce(): Promise<void | { err: unknown }> {
     try {
       this._config.isWorker = false;
       if (!this._config.socket) {
@@ -220,7 +215,7 @@ export default class Delcom {
       }
       await this._config.socket.emitWithAck('leave_ack');
     } catch (err) {
-      return {err};
+      return { err };
     }
   }
 
@@ -228,15 +223,15 @@ export default class Delcom {
    *
    * @returns
    */
-  async getWorkers(): Promise<{res?: workerListElement[], err?: unknown}> {
+  async getWorkers(): Promise<{ res?: workerListElement[]; err?: unknown }> {
     try {
       const socket = this._config.socket;
       if (!socket) {
         throw Error('Cannot get workers, no socket!');
       }
-      return {res: await socket.emitWithAck('get_workers_ack')};
+      return { res: await socket.emitWithAck('get_workers_ack') };
     } catch (err) {
-      return {err};
+      return { err };
     }
   }
 
@@ -253,11 +248,11 @@ export default class Delcom {
     filePaths: fs.PathLike[],
     outDir?: fs.PathLike,
     cbs?: {
-      cb1?: ()=>unknown, // called after job created with worker
-      cb2?: ()=>unknown, // called after job files sent
-      cb3?: ()=>unknown, // called after job completed
-    },
-  ): Promise<void | {err: unknown}> {
+      cb1?: () => unknown; // called after job created with worker
+      cb2?: () => unknown; // called after job files sent
+      cb3?: () => unknown; // called after job completed
+    }
+  ): Promise<void | { err: unknown }> {
     try {
       await this.createJob(workerID, filePaths, outDir);
       if (cbs?.cb1) cbs.cb1();
@@ -266,7 +261,7 @@ export default class Delcom {
       await this._config.res?.finishPromise.promise;
       if (cbs?.cb3) cbs?.cb3();
     } catch (err) {
-      return {err};
+      return { err };
     }
   }
 
@@ -281,10 +276,12 @@ export default class Delcom {
       const dir = this._config.job?.dir.toString();
       const socket = this._config.socket;
       const dockerName = path.basename(dir).toLowerCase();
-      const build = spawn(
-        'docker',
-        ['build', `-t${dockerName}`, '--progress=plain', dir],
-      );
+      const build = spawn('docker', [
+        'build',
+        `-t${dockerName}`,
+        '--progress=plain',
+        dir,
+      ]);
       build.stdout.on('data', (chunk) => {
         socket.emit('build_std_out', chunk);
       });
@@ -312,10 +309,7 @@ export default class Delcom {
       const dir = this._config.job?.dir.toString();
       const socket = this._config.socket;
       const dockerName = path.basename(dir).toLowerCase();
-      const build = spawn(
-        'docker',
-        ['run', dockerName],
-      );
+      const build = spawn('docker', ['run', dockerName]);
       build.stdout.on('data', (chunk) => {
         socket.emit('run_std_out', chunk);
       });
@@ -335,12 +329,15 @@ export default class Delcom {
   private async createJob(
     workerID: string,
     filePaths: fs.PathLike[],
-    outDir?: fs.PathLike,
+    outDir?: fs.PathLike
   ) {
     try {
       this._config.isDelegating = true;
       this._config.res = {
-        dir: outDir ? outDir : await fsp.mkdtemp(`${this._config.delcomTempDir}${path.sep}res`),
+        dir:
+          outDir ? outDir : (
+            await fsp.mkdtemp(`${this._config.delcomTempDir}${path.sep}res`)
+          ),
         writeStreams: {},
         finishPromise: {},
       };
@@ -352,22 +349,28 @@ export default class Delcom {
       });
       for (const outputName of outputNames) {
         this._config.res.writeStreams[outputName] = fs.createWriteStream(
-          `${this._config.res.dir}${path.sep}${outputName}`,
+          `${this._config.res.dir}${path.sep}${outputName}`
         );
       }
       if (!(await fsp.lstat(this._config.res.dir)).isDirectory()) {
         throw Error(`outDir is not a valid directory! Was given ${outDir}`);
       }
-      const fileNames = await Promise.all(filePaths.map(async (filePath) => {
-        if (!(await fsp.lstat(filePath)).isFile()) {
-          throw Error(`Invalid file path! ${filePath} is not a file!`);
-        }
-        return path.basename(filePath.toString());
-      }));
+      const fileNames = await Promise.all(
+        filePaths.map(async (filePath) => {
+          if (!(await fsp.lstat(filePath)).isFile()) {
+            throw Error(`Invalid file path! ${filePath} is not a file!`);
+          }
+          return path.basename(filePath.toString());
+        })
+      );
       if (!fileNames.includes('Dockerfile')) {
         throw Error('No Dockerfile found in filePaths!');
       }
-      const ack = await this._config.socket?.emitWithAck('new_job_ack', workerID, fileNames);
+      const ack = await this._config.socket?.emitWithAck(
+        'new_job_ack',
+        workerID,
+        fileNames
+      );
       if (ack) {
         throw ack;
       }
@@ -383,21 +386,25 @@ export default class Delcom {
       if (!socket) {
         throw Error('No socket to send through!');
       }
-      await Promise.all(filePaths.map(async (filePath) => {
-        const name = path.basename(filePath.toString());
-        const readStream = fs.createReadStream(filePath, {encoding: 'base64'});
-        readStream.on('data', async (chunk) => {
-          await socket.emitWithAck('send_file_data_ack', {name, chunk});
-        });
-        return new Promise<void>((resolve, reject) => {
-          readStream.on('close', () => {
-            if (readStream.errored) {
-              reject(`readStream for ${name} errored`);
-            }
-            resolve();
+      await Promise.all(
+        filePaths.map(async (filePath) => {
+          const name = path.basename(filePath.toString());
+          const readStream = fs.createReadStream(filePath, {
+            encoding: 'base64',
           });
-        });
-      }));
+          readStream.on('data', async (chunk) => {
+            await socket.emitWithAck('send_file_data_ack', { name, chunk });
+          });
+          return new Promise<void>((resolve, reject) => {
+            readStream.on('close', () => {
+              if (readStream.errored) {
+                reject(`readStream for ${name} errored`);
+              }
+              resolve();
+            });
+          });
+        })
+      );
       console.log('files done sending');
       socket.emit('files_done_sending');
     } catch (err) {
@@ -414,11 +421,11 @@ export default class Delcom {
     }
     if (callback) {
       if (err instanceof Error) {
-        callback({err: err.message});
+        callback({ err: err.message });
       } else if (typeof err == 'string') {
-        callback({err});
+        callback({ err });
       } else if (err) {
-        callback ({err: 'Unknown error'});
+        callback({ err: 'Unknown error' });
       } else {
         callback();
       }
